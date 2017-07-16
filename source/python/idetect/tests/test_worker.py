04686b9b0c03b76fb518c44c4d293d5634db97f4
WanderingStar <github@account.loath.org>
2017-07-16 16:05:20 -0500

    Parallel workers

--- source/python/idetect/tests/test_worker.py
+++ source/python/idetect/tests/test_worker.py
@@ -1,89 +1,131 @@
+import logging
 import os
-from datetime import datetime
-from unittest import TestCase
 import random
 import time
+from datetime import datetime
+from multiprocessing import Process
+from unittest import TestCase
 
-import sqlalchemy
 from sqlalchemy import create_engine
 
-from idetect.model import Base, Session, Status, Article, UnexpectedArticleStatusException
+from idetect.model import Base, Session, Status, Article
 from idetect.worker import Worker
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 class TestWorker(TestCase):
     def setUp(self):
+        worker_logger = logging.getLogger("idetect.worker")
+        worker_logger.setLevel(logging.INFO)
+
         db_host = os.environ.get('DB_HOST')
         db_url = 'postgresql://{user}:{passwd}@{db_host}/{db}'.format(
             user='tester', passwd='tester', db_host=db_host, db='idetect_test')
-        engine = create_engine(db_url)
-        Session.configure(bind=engine)
-        Base.metadata.create_all(engine)
+        self.engine = create_engine(db_url)
+        Session.configure(bind=self.engine)
+        Base.metadata.create_all(self.engine)
         self.session = Session()
+        self.processes = []
+        logging.basicConfig()
 
     def tearDown(self):
+        for process in self.processes:
+            logger.debug(f"Terminating {process}")
+            process.terminate()
         self.session.rollback()
-        version = sqlalchemy.__version__
-        self.session.query(Article).filter(Article.url =='http://example.com').delete()
-        self.session.commit()
+        if self.session.query(Article).filter(Article.url == 'http://example.com').delete() > 0:
+            self.session.commit()
 
     @staticmethod
     def nap_fn(article):
         time.sleep(random.randrange(1))
 
     def test_work_one(self):
-        worker = Worker(Status.NEW, Status.FETCHING, Status.FETCHED, Status.FETCHING_FAILED, TestWorker.nap_fn)
+        worker = Worker(Status.NEW, Status.FETCHING, Status.FETCHED, Status.FETCHING_FAILED,
+                        TestWorker.nap_fn, self.engine)
         article = Article(url='http://example.com', status=Status.NEW)
         self.session.add(article)
         self.session.commit()
-        self.assertTrue(worker.work(self.session), "Worker didn't find work")
+        self.assertTrue(worker.work(), "Worker didn't find work")
 
         article2 = self.session.query(Article).get(article.id)
         self.assertEqual(article2.status, Status.FETCHED)
 
-        self.assertFalse(worker.work(self.session), "Worker found work")
+        self.assertFalse(worker.work(), "Worker found work")
 
     @staticmethod
     def err_fn(article):
         raise RuntimeError("Nope")
 
     def test_work_failure(self):
-        worker = Worker(Status.NEW, Status.FETCHING, Status.FETCHED, Status.FETCHING_FAILED, TestWorker.err_fn)
+        worker = Worker(Status.NEW, Status.FETCHING, Status.FETCHED, Status.FETCHING_FAILED,
+                        TestWorker.err_fn, self.engine)
         article = Article(url='http://example.com', status=Status.NEW)
         self.session.add(article)
         self.session.commit()
-        self.assertTrue(worker.work(self.session), "Worker didn't find work")
+        self.assertTrue(worker.work(), "Worker didn't find work")
 
         article2 = self.session.query(Article).get(article.id)
         self.assertEqual(article2.status, Status.FETCHING_FAILED)
 
-        self.assertFalse(worker.work(self.session), "Worker found work")
+        self.assertFalse(worker.work(), "Worker found work")
 
     def test_work_chain(self):
-        worker1 = Worker(Status.NEW, Status.FETCHING, Status.FETCHED, Status.FETCHING_FAILED, TestWorker.nap_fn)
-        worker2 = Worker(Status.FETCHED, Status.PROCESSING, Status.PROCESSED, Status.PROCESSING_FAILED, TestWorker.nap_fn)
+        worker1 = Worker(Status.NEW, Status.FETCHING, Status.FETCHED, Status.FETCHING_FAILED,
+                         TestWorker.nap_fn, self.engine)
+        worker2 = Worker(Status.FETCHED, Status.PROCESSING, Status.PROCESSED, Status.PROCESSING_FAILED,
+                         TestWorker.nap_fn, self.engine)
         article = Article(url='http://example.com', status=Status.NEW)
         self.session.add(article)
         self.session.commit()
-        self.assertFalse(worker2.work(self.session), "Worker2 found work")
-        self.assertTrue(worker1.work(self.session), "Worker didn't find work")
-        self.assertTrue(worker2.work(self.session), "Worker didn't find work")
+        self.assertFalse(worker2.work(), "Worker2 found work")
+        self.assertTrue(worker1.work(), "Worker didn't find work")
+        self.assertTrue(worker2.work(), "Worker didn't find work")
 
         article2 = self.session.query(Article).get(article.id)
         self.assertEqual(article2.status, Status.PROCESSED)
 
-        self.assertFalse(worker1.work(self.session), "Worker1 found work")
-        self.assertFalse(worker2.work(self.session), "Worker2 found work")
+        self.assertFalse(worker1.work(), "Worker1 found work")
+        self.assertFalse(worker2.work(), "Worker2 found work")
 
     def test_work_all(self):
-        worker = Worker(Status.NEW, Status.FETCHING, Status.FETCHED, Status.FETCHING_FAILED, TestWorker.nap_fn)
+        worker = Worker(Status.NEW, Status.FETCHING, Status.FETCHED, Status.FETCHING_FAILED,
+                        TestWorker.nap_fn, self.engine)
         n = 3
         for i in range(n):
             article = Article(url='http://example.com', status=Status.NEW)
             self.session.add(article)
             self.session.commit()
-        self.assertEqual(worker.work_all(self.session), 3)
+        self.assertEqual(worker.work_all(), 3)
 
-        self.assertEqual(self.session.query(Article).filter(Article.status==Status.NEW).count(), 0)
-        self.assertEqual(self.session.query(Article).filter(Article.status==Status.FETCHED).count(), n)
+        self.assertEqual(self.session.query(Article).filter(Article.status == Status.NEW).count(), 0)
+        self.assertEqual(self.session.query(Article).filter(Article.status == Status.FETCHED).count(), n)
 
+    def test_work_parallel(self):
+        n = 1000
+        for i in range(n):
+            article = Article(url='http://example.com', status=Status.NEW)
+            self.session.add(article)
+            self.session.commit()
+        p = 4
+        for i in range(p):
+            worker = Worker(Status.NEW, Status.FETCHING, Status.FETCHED, Status.FETCHING_FAILED,
+                            TestWorker.nap_fn, self.engine)
+            process = Process(target=worker.work_indefinitely, daemon=True)
+            self.processes.append(process)
+            process.start()
+        self.engine.dispose()
+        self.session = Session()
+        start = datetime.now()
+        for i in range(int(n / p)):  # shouldn't take longer than this...
+            remaining = self.session.query(Article).filter(Article.status == Status.NEW).count()
+            if remaining == 0:
+                logger.info(f"Processing took {datetime.now() - start}")
+                break
+            logger.info(f"{remaining} remain after {datetime.now() - start}")
+            time.sleep(1)
+        else:
+            logger.info(f"Processing took {datetime.now() - start} seconds!.")
+        time.sleep(1)
--- source/python/idetect/worker.py
+++ source/python/idetect/worker.py
@@ -1,11 +1,15 @@
-from idetect.model import Article, UnexpectedArticleStatusException
 import logging
+import os
+import signal
+import time
+
+from idetect.model import Article, UnexpectedArticleStatusException, Session
 
 logger = logging.getLogger(__name__)
 
-class Worker:
 
-    def __init__(self, status, working_status, success_status, failure_status, function):
+class Worker:
+    def __init__(self, status, working_status, success_status, failure_status, function, engine):
         """
         Create a Worker that looks for Articles with a given status. When it finds one, it marks it with 
         working_status and runs a function. If the function returns without an exception, it advances the Article to 
@@ -16,41 +20,68 @@ class Worker:
         self.success_status = success_status
         self.failure_status = failure_status
         self.function = function
+        self.engine = engine
+        self.session = None
+        self.terminated = False
+        signal.signal(signal.SIGINT, self.terminate)
+        signal.signal(signal.SIGTERM, self.terminate)
 
-    def work(self, session):
+    def terminate(self, signum, frame):
+        print(f"Worker {os.getpid()} terminated")
+        self.terminated = True
+
+    def work(self):
         """
         Look for articles in the given session and run function on them
         if any are found, managing status appropriately. Return True iff some Articles were processed (successfully or not)
         """
-        claimed_one = False
-        article = None
-        while not claimed_one:
-            try:
-                article = session.query(Article).filter(Article.status==self.status)\
-                    .order_by(Article.updated).first() # choose the least-recently-updated article with this status
-                if article is None:
-                    return False  # no work to be done
-                article.update_status(self.working_status)
-                session.commit()
-                logger.info(f"Worker claimed Article {article.id} in status {self.status}")
-                claimed_one = True
-            except UnexpectedArticleStatusException:
-                pass  # another worker claimed this article before we could, try again
         try:
-            self.function(article)
-            logger.info(f"Worker processed Article {article.id} {self.status} -> {self.success_status}")
-            article.update_status(self.success_status)
-            session.commit()
-        except Exception as e:
-            logger.warn(f"Worker failed to process Article {article.id} {self.status} -> {self.failure_status}",
-                        exc_info=e)
-            article.update_status(self.failure_status)
-            session.commit()
-        return True
+            if self.session is None:
+                self.engine.dispose()  # each Worker must have its own session, made in-Process
+                Session.configure(bind=self.engine)
+                self.session = Session()
+            claimed_one = False
+            article = None
+            while not claimed_one:
+                try:
+                    article = self.session.query(Article).filter(Article.status == self.status) \
+                        .order_by(Article.updated).first()  # choose the least-recently-updated article with this status
+                    if article is None:
+                        return False  # no work to be done
+                    article.update_status(self.working_status)
+                    self.session.commit()
+                    logger.info(f"Worker claimed Article {article.id} in status {self.status}")
+                    claimed_one = True
+                except UnexpectedArticleStatusException:
+                    pass  # another worker claimed this article before we could, try again
+            try:
+                self.function(article)
+                logger.info(f"Worker processed Article {article.id} {self.status} -> {self.success_status}")
+                article.update_status(self.success_status)
+                self.session.commit()
+            except Exception as e:
+                logger.warn(f"Worker failed to process Article {article.id} {self.status} -> {self.failure_status}",
+                            exc_info=e)
+                article.update_status(self.failure_status)
+                self.session.commit()
+            return True
+        finally:
+            if self.session is not None:
+                self.session.rollback()
 
-    def work_all(self, session):
+    def work_all(self):
         """Work repeatedly until there is no work to do. Return a count of the number of units of work done"""
         count = 0
-        while self.work(session):
+        while self.work() and not self.terminated:
             count += 1
         return count
+
+    def work_indefinitely(self, max_sleep=60):
+        """While there is work to do, do it. If there's no work to do, take increasingly long naps until there is."""
+        sleep = 1
+        while not self.terminated:
+            if self.work_all() > 0:
+                sleep = 1
+            else:
+                time.sleep(sleep)
+                sleep = min(max_sleep, sleep * 2)
