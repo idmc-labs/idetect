4a221cc8e8d0b230f4daf6111420871330ea25d1
Simon Bedford <simon.bedford@gmail.com>
2017-07-18 18:49:05 -0500

    add back in modified fact extractor and interpreter

--- source/python/idetect/fact_extractor.py
+++ source/python/idetect/fact_extractor.py
@@ -2,9 +2,13 @@
 
 How to ensure has access to pre-loaded models?
 '''
-from flask import Blueprint, render_template
+from flask import Blueprint
+from app.interpreter import Interpreter, person_reporting_terms, structure_reporting_terms, person_reporting_units, \
+    structure_reporting_units, relevant_article_terms
+# from app import nlp
 
 extractor_api = Blueprint('extractor_api', __name__)
+
 
 @extractor_api.route('/extract', methods=['GET'])
 def extract():
@@ -13,8 +17,11 @@ def extract():
     # For each piece of content, run fact extraction
 
     # Save extracted facts in Fact Database
-    return render_template('success.html', endpoint=__name__)
+    pass
 
 
 def extract_facts(content):
-    pass
+    # Get rules-based facts along with sentence numbers
+    interpreter = Interpreter(nlp, person_reporting_terms, structure_reporting_terms, person_reporting_units,
+                              structure_reporting_units, relevant_article_terms)
+    rules_based_reports = interpreter.process_article_new(interpreter)
