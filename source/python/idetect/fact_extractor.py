d099fe291261878a3901b1f8f1a23344b0e5ab37
Simon Bedford <simon.bedford@gmail.com>
2017-07-18 19:47:45 -0500

    error fix in extract_facts

--- source/python/idetect/fact_extractor.py
+++ source/python/idetect/fact_extractor.py
@@ -2,26 +2,17 @@
 
 How to ensure has access to pre-loaded models?
 '''
-from flask import Blueprint
-from app.interpreter import Interpreter, person_reporting_terms, structure_reporting_terms, person_reporting_units, \
+from interpreter import Interpreter, person_reporting_terms, structure_reporting_terms, person_reporting_units, \
     structure_reporting_units, relevant_article_terms
-# from app import nlp
+import spacy
 
-extractor_api = Blueprint('extractor_api', __name__)
-
-
-@extractor_api.route('/extract', methods=['GET'])
-def extract():
-    # Query URL Database for relevant content
-
-    # For each piece of content, run fact extraction
-
-    # Save extracted facts in Fact Database
-    pass
+nlp = spacy.load("en")
+print("Loaded Spacy English Language NLP Models.")
 
 
 def extract_facts(content):
     # Get rules-based facts along with sentence numbers
     interpreter = Interpreter(nlp, person_reporting_terms, structure_reporting_terms, person_reporting_units,
                               structure_reporting_units, relevant_article_terms)
-    rules_based_reports = interpreter.process_article_new(interpreter)
+    reports = interpreter.process_article_new(content)
+    return reports
