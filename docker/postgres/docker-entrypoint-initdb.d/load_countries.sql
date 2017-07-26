1824c6e6849c4d1a39349769992441d3c47c6124
Simon Bedford <simon.bedford@gmail.com>
2017-07-26 09:43:37 -0500

    Change country pre-loading from sql to python script

--- docker/postgres/docker-entrypoint-initdb.d/load_countries.sql
+++
@@ -1,5 +0,0 @@
-\copy country(code, preferred_term) FROM '/home/idetect/data/countries.csv' DELIMITER ',' CSV HEADER;
-
-\copy country_term(term, country) FROM '/home/idetect/data/country_terms.csv' DELIMITER ',' CSV HEADER;
-
-\copy location(description, location_type, country_code, latlong) FROM '/home/idetect/data/country_locations.csv' DELIMITER ',' CSV HEADER;
