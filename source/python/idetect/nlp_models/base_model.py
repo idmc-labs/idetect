dcc909c5d273888778f0d267d7421a530d0e4b87
George Richardson <g.raymond.richardson@gmail.com>
2017-07-30 11:04:04 -0700

    simplify tfidf and lis transformers

--- source/python/idetect/nlp_models/base_model.py
+++ source/python/idetect/nlp_models/base_model.py
@@ -8,10 +8,12 @@ from sklearn.externals import joblib
 
 class DownloadableModel(object):
 
-    """Summary
+    """A base class for loading pickeld scikit-learn models that may be stored
+    locally or in online storage.
 
     Attributes:
-        model (sklearn model): a
+        model (sklearn model): a scikit-learn Transformer, Estimator, or
+            Pipeline, which has the "predict" method.
     """
 
     def __init__(self, model_path, model_url):
@@ -28,7 +30,7 @@ class DownloadableModel(object):
             model_url (str): URL where a model lives in storage.
 
         Returns:
-            clf (sklearn model): An unpickled sklearn model (Transformer,
+            model (sklearn model): An unpickled sklearn model (Transformer,
                 Estimator, or Pipeline).
         """
         # Load users model if specified and exists
@@ -39,7 +41,7 @@ class DownloadableModel(object):
                 try:
                     fcntl.flock(f, fcntl.LOCK_EX)
                     if os.path.getsize(model_path) > 0:
-                        clf = joblib.load(model_path)
+                        model = joblib.load(model_path)
                 except IOError as e:
                     if e.errno != errno.EAGAIN:
                         raise
@@ -47,12 +49,13 @@ class DownloadableModel(object):
                     fcntl.flock(f, fcntl.LOCK_UN)
         else:  # download file
             # if file directory doesn't exist create it
-            if not os.path.dirname(model_path):
-                try:
-                    os.makedirs(os.path.dirname(model_path))
-                except OSError as exc:  # Guard against rare condition
-                    if exc.errno != errno.EEXIST:
-                        raise
+            if os.path.dirname(model_path):
+                if not os.path.isdir(os.path.dirname(model_path)):
+                    try:
+                        os.makedirs(os.path.dirname(model_path))
+                    except OSError as exc:  # Guard against rare condition
+                        if exc.errno != errno.EEXIST:
+                            raise
             with open(model_path, 'wb+') as f:
                 try:
                     fcntl.flock(f, fcntl.LOCK_EX | fcntl.LOCK_NB)
@@ -65,10 +68,13 @@ class DownloadableModel(object):
                     fcntl.flock(f, fcntl.LOCK_EX)
                 finally:
                     fcntl.flock(f, fcntl.LOCK_UN)
-            clf = joblib.load(model_path)
-        return clf
+            model = joblib.load(model_path)
+        return model
 
     def predict(self, text):
         """ This method should be overwritten to fit the specific case of the
         model being used """
-        return self.model.transform(pd.Series(text))[0]
+        try:
+            return self.model.transform(pd.Series(text))[0]
+        except ValueError:
+            raise
--- source/python/idetect/nlp_models/relevance/relevance.py
+++ source/python/idetect/nlp_models/relevance/relevance.py
@@ -1,186 +1,150 @@
-import os
-import requests
 import gensim
 import pandas as pd
 import numpy as np
 from nltk.tokenize import WordPunctTokenizer
+from nltk.stem import PorterStemmer
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.externals import joblib
 
 from idetect.model import Relevance
+from idetect.nlp_models.base_model import DownloadableModel
 
-class RelevanceModel(object):
-    def __init__(self, model_path=None):
-        self.model = self.load_model(model_path=model_path)
 
-    def load_model(self, model_path=None):
-        if model_path and os.path.isfile(model_path):
-            clf = joblib.load(model_path)
-        else:
-            default_model_path = 'relevance_classifier.pkl'
-            if os.path.isfile(default_model_path):
-                clf = joblib.load(default_model_path)
-            else:
-                url = 'https://s3-us-west-2.amazonaws.com/idmc-idetect/relevance_models/relevance_classifier.pkl'
-                r = requests.get(url, stream=True)
-                if not os.path.isfile(default_model_path):
-                    try:
-                        os.makedirs(os.path.dirname(default_model_path))
-                    except OSError as exc: # Guard against race condition
-                        if exc.errno != errno.EEXIST:
-                            raise
-                with open(default_model_path, 'wb') as f:
-                    for chunk in r.iter_content(chunk_size=1024):
-                        if chunk:  # filter out keep-alive new chunks
-                            f.write(chunk)
-                clf = joblib.load(default_model_path)
-        return clf
+class RelevanceModel(DownloadableModel):
 
     def predict(self, text):
         try:
             relevance = self.model.transform(pd.Series(text))[0]
-        except:
-            # if error occurs, classify as most likely category
-            relevance = 'yes'
+            if relevance == 'yes':
+                return Relevance.DISPLACEMENT
+            else:
+                return Relevance.NOT_DISPLACEMENT
+        except ValueError:
+            # error can occur if empty text is passed to model
+            raise
 
-        if relevance == 'yes':
-            return Relevance.DISPLACEMENT
-        else:
-            return Relevance.NOT_DISPLACEMENT
 
 class Tokenizer(TransformerMixin):
     def __init__(self, stop_words=None):
         self.stop_words = stop_words
         self.tokenizer = WordPunctTokenizer()
-        
+
     def prepare_tokens(self, text, stop_words):
         tokens = self.tokenizer.tokenize(text)
         tokens = [t for t in tokens if len(t) > 2]
-        tokens = [t for t in tokens if t not in stop_words]
         tokens = [t.lower() for t in tokens]
+        tokens = [t for t in tokens if t not in stop_words]
         tokens = [t for t in tokens if not t.isdigit()]
         return tokens
-    
+
     def fit(self, X, *args):
         return self
-    
+
     def transform(self, X, *args):
         X = X.map(lambda x: self.prepare_tokens(x, self.stop_words))
         return X
+
 
 class Stemmer(TransformerMixin):
     def __init__(self, stop_words):
         self.stop_words = stop_words
         self.tokenizer = WordPunctTokenizer()
         self.stemmer = PorterStemmer()
-        
+
     def prepare_stems(self, text, stop_words):
         tokens = self.tokenizer.tokenize(text)
         tokens = [t for t in tokens if len(t) > 2]
-        tokens = [t for t in tokens if t not in stop_words] 
+        tokens = [t.lower() for t in tokens]
+        tokens = [t for t in tokens if t not in stop_words]
         stems = [self.stemmer.stem(t) for t in tokens]
-        stems = [s.lower() for s in stems]
         stems = [s for s in stems if not s.isdigit()]
         return stems
-    
+
     def fit(self, X, *args):
         return self
-    
+
     def transform(self, X, *args):
         X = X.map(lambda x: self.prepare_stems(x, self.stop_words))
         return X
 
+
 class TfidfTransformer(TransformerMixin):
-    def __init__(self, no_below=5, no_above=0.5, tfidf_model=None, dictionary=None):
-        self.dictionary = dictionary
-        self.tfidf_model = tfidf_model
+    def __init__(self, no_below=5, no_above=0.5):
         self.no_below = no_below
         self.no_above = no_above
-    
-    def make_dictionary(self, texts):
-        if not self.dictionary:
-            self.dictionary = gensim.corpora.Dictionary(texts)
-            if self.no_below or self.no_above:
-                self.dictionary.filter_extremes(no_below=self.no_below, no_above=self.no_above)
-        return self
-    
-    def tfidf_transform(texts, dictionary=None, tfidf_model=None):
-        if not dictionary:
-            dictionary = gensim.corpora.Dictionary(texts)
-        corpus = [dictionary.doc2bow(text) for text in texts]
-        if not tfidf_model:
-            tfidf_model = gensim.models.TfidfModel(corpus)
-        corpus_tfidf = tfidf_model[corpus]
-        return corpus_tfidf, dictionary, tfidf_model
-    
+
+    def set_dictionary(self, texts):
+        self.dictionary = gensim.corpora.Dictionary(texts)
+        if self.no_below or self.no_above:
+            self.dictionary.filter_extremes(no_below=self.no_below,
+                                            no_above=self.no_above)
+
     def make_corpus(self, texts):
         corpus = [self.dictionary.doc2bow(text) for text in texts]
         return corpus
-    
-    def make_tfidf_model(self, corpus):
-        if not self.tfidf_model:
-            self.tfidf_model = gensim.models.TfidfModel(corpus)
-        return self
-    
+
+    def set_tfidf_model(self, corpus):
+        self.tfidf_model = gensim.models.TfidfModel(corpus)
+
     def fit(self, texts, y=None):
-        self.make_dictionary(texts)
-        self.corpus = self.make_corpus(texts)
-        self.make_tfidf_model(self.corpus)
-        
+        self.set_dictionary(texts)
+        corpus = self.make_corpus(texts)
+        self.set_tfidf_model(corpus)
+        return self
+
     def transform(self, texts):
         corpus = self.make_corpus(texts)
         return self.tfidf_model[corpus]
 
+
 class LsiTransformer(TransformerMixin):
-    def __init__(self, n_dimensions=100, no_below=5, no_above=0.5, lsi_model=None):
-        self.lsi_model = lsi_model
+    def __init__(self, n_dimensions=100, no_below=5, no_above=0.5):
         self.n_dimensions = n_dimensions
         self.no_below = no_below
         self.no_above = no_above
-    
-    def build_tfidf(self, texts):
-        self.tfidf_transformer = TfidfTransformer(no_below=self.no_below, no_above=self.no_above)
-        self.tfidf_transformer.fit(texts)
-        corpus_tfidf = self.tfidf_transformer.transform(texts)
-        dictionary = self.tfidf_transformer.dictionary
-        return corpus_tfidf, dictionary
-    
+
     def lsi_to_vecs(self, corpus_lsi):
         lsi_vecs = []
         for c in corpus_lsi:
             vec = [x[1] for x in c]
             lsi_vecs.append(vec)
         return np.array(lsi_vecs)
-    
-    def make_lsi_model(self, texts):    
-        self.corpus_tfidf, self.dictionary = self.build_tfidf(texts)
-        if not self.lsi_model:
-            self.lsi_model = gensim.models.LsiModel(self.corpus_tfidf, 
-                                                    id2word=self.dictionary, 
-                                                    num_topics=self.n_dimensions)
-        return self
-    
+
+    def make_tfidf(self, texts):
+        self.tfidf_transformer = TfidfTransformer(no_below=self.no_below,
+                                                  no_above=self.no_above)
+        self.tfidf_transformer.fit(texts)
+        corpus_tfidf = self.tfidf_transformer.transform(texts)
+        dictionary = self.tfidf_transformer.dictionary
+        return corpus_tfidf, dictionary
+
     def make_corpus(self, corpus_tfidf):
         lsi_corpus = self.lsi_model[corpus_tfidf]
         return lsi_corpus
-    
+
+    def set_lsi_model(self, texts):
+        corpus_tfidf, dictionary = self.make_tfidf(texts)
+        self.lsi_model = gensim.models.LsiModel(corpus_tfidf,
+                                            id2word=dictionary,
+                                            num_topics=self.n_dimensions)
+
     def fit(self, texts, *args, **kwargs):
-        self.make_lsi_model(texts)
+        self.set_lsi_model(texts)
         self.corpus_lsi = self.lsi_model[self.corpus_tfidf]
         return self
-    
+
     def transform(self, texts):
-        corpus_tfidf = self.tfidf_transformer.transform(texts)
+        corpus_tfidf, _dictionary = self.make_tfidf(texts)
         corpus_lsi = self.make_corpus(corpus_tfidf)
         return self.lsi_to_vecs(corpus_lsi)
-        
+
+
 class Combiner(BaseEstimator, TransformerMixin):
     def __init__(self, ml_model, kw_model):
         self.ml_model = ml_model
         self.kw_model = kw_model
-        
+
     def combine_relevance_tags(self, classified, keyword_tagged):
         combined = []
         for classifier, keyword in zip(classified, keyword_tagged):
@@ -194,10 +158,10 @@ class Combiner(BaseEstimator, TransformerMixin):
                 tag = 'yes'
             combined.append(tag)
         return combined
-    
+
     def fit(self, X, y=None):
         return self
-        
+
     def transform(self, X, *args):
         ml_tagged = self.ml_model.predict(X)
         kw_tagged = self.kw_model.transform(X)
