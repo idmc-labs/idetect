8965ba18410f6aacdcf24ec9a4953a4e1d83ce07
Simon Bedford <simon.bedford@gmail.com>
2017-08-19 09:17:53 -0500

    Align Fact/Country data model and fact extraction more with IDETECT database schema

--- source/python/idetect/fact_extractor.py
+++ source/python/idetect/fact_extractor.py
@@ -15,51 +15,57 @@ nlp = spacy.load("en_default")
 print("Loaded Spacy English Language NLP Models.")
 
 
-def extract_facts(article):
-    '''Extract facts (facts) for given article
-    :params article: instance of Article
+def extract_facts(analysis):
+    '''Extract facts (facts) for given instance of Analysis
+    :params article: instance of Analysis
     :return: None
     '''
-    session = object_session(article)
+    session = object_session(analysis)
     interpreter = Interpreter(session, nlp)
-    content = article.content.content
+    content = analysis.content.content
     facts = interpreter.process_article_new(content)
     if len(facts) > 0:
-        save_facts(article, facts, session)
+        save_facts(analysis, facts, session)
 
 
-def save_facts(article, facts, session):
+def save_facts(analysis, facts, session):
     '''Loop through extracted facts and save them to database
     :params article: instance of Article
     :params facts: list of extracted facts
     :params session: session object corresponding to the article
     :return: None
     '''
-    for r in facts:
-        fact = Fact(article_id=article.id, reporting_unit=r.reporting_unit, reporting_term=r.reporting_term,
-                    sentence_start=r.sentence_start, sentence_end=r.sentence_end,
-                    specific_displacement_figure=r.quantity[0],
-                    vague_displacement_figure=r.quantity[1],
+    for f in facts:
+        # First geolocate locations; split into countries and create one fact per country
+        country_locations = []
+        for location in f.locations:
+            country_locations.append((process_location(location, session)))
+
+        country_locations.sort(key=lambda x: x.country)
+        for key, group in groupby(country_locations, lambda x: x.country):
+
+            fact = Fact(article_id=analysis.document_id, unit=f.reporting_unit, term=f.reporting_term,
+                    excerpt_start=f.sentence_start, excerpt_end=f.sentence_end,
+                    specific_reported_figure=f.quantity[0],
+                    vague_reported_figure=f.quantity[1],
                     tag_locations=json.dumps(r.tag_spans))
-        session.add(fact)
-        session.commit()
-
-        # Loop over each extracted location and save to Database
-        for location in r.locations:
-            process_location(fact, location, session)
+            session.add(fact)
+            session.commit()
+            fact.locations.extend([location for location in group])
 
 
-def process_location(fact, location, session):
+def process_location(location_name, session):
     '''Get geo info for a given location and add the location to database
     :params fact: instance of Fact
     :params location: location name, a String
     :params session: session object corresponding to location
     :return: None
     '''
-    loc = session.query(Location).filter_by(
-        description=location).one_or_none()
-    if loc:
-        fact.locations.append(loc)
+    locations = []
+    location = session.query(Location).filter_by(
+        description=location_name).one_or_none()
+    if location:
+        locations.append(location)
     else:
         loc_info = get_geo_info(location)
         if loc_info['flag'] != 'no-results':
@@ -70,4 +76,5 @@ def process_location(fact, location, session):
                                 country=country, latlong=loc_info['coordinates'])
             session.add(location)
             session.commit()
-            fact.locations.append(location)
+            locations.append(location)
+    return locations
