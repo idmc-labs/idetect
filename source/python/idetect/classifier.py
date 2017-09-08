2781aed8c2df6242a59250cc6f30ebd1aca62563
George Richardson <g.raymond.richardson@gmail.com>
2017-09-07 20:54:56 -0700

    change classifier from article to analysis

--- source/python/idetect/classifier.py
+++ source/python/idetect/classifier.py
@@ -4,16 +4,16 @@ from sqlalchemy.orm import object_session
 '''Method(s) for running classifier on extracted content.
 '''
 
-def classify(article, category_model, relevance_model):
+def classify(analysis, category_model, relevance_model):
     """
     Classify article into category & relevance and save results to database
     :params article: An Article instance
     :return: None
     """
-    session = object_session(article)
-    content = article.content.content
+    session = object_session(analysis)
+    content = analysis.content.content
     category = category_model.predict(content)
     relevance = relevance_model.transform(content)
-    article.category = category
-    article.relevance = relevance
+    analysis.category = category
+    analysis.relevance = relevance
     session.commit()
