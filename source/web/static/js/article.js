e6486afe6e203f1a479c3158744bf8f20a886371
Simon Bedford <simon.bedford@gmail.com>
2017-09-12 15:53:14 -0500

    Some cosmetic changes

--- source/web/static/js/article.js
+++ source/web/static/js/article.js
@@ -1,6 +1,11 @@
 $(document).ready(function(){
 
-	var mymap = L.map('mapid').setView(coords[0], 3);
+	if (coords.length > 0){
+		var mymap = L.map('mapid').setView(coords[0], 3);
+	}
+	else{
+		var mymap = L.map('mapid').setView([0, 0], 3);
+	}
 
 	var layer = L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
   		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attributions">CARTO</a>'
