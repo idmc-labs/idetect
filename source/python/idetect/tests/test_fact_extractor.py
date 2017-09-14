8df10bf0f75fa17b12c4625e514599b546c6f1d2
Simon Bedford <simon.bedford@gmail.com>
2017-09-14 16:03:53 -0500

    Add tests for extract_facts function

---
+++ source/python/idetect/tests/test_fact_extractor.py
@@ -0,0 +1,106 @@
+import os
+from unittest import TestCase
+
+from sqlalchemy import create_engine
+
+from idetect.model import Base, Session, Status, Document, Analysis, DocumentContent, DocumentType
+from idetect.fact_extractor import extract_facts
+from idetect.load_data import load_countries, load_terms
+
+
+class TestFactExtractor(TestCase):
+
+    def setUp(self):
+        db_host = os.environ.get('DB_HOST')
+        db_url = 'postgresql://{user}:{passwd}@{db_host}/{db}'.format(
+            user='tester', passwd='tester', db_host=db_host, db='idetect_test')
+        engine = create_engine(db_url)
+        Session.configure(bind=engine)
+        Base.metadata.drop_all(engine)
+        Base.metadata.create_all(engine)
+        self.session = Session()
+        load_countries(self.session)
+        load_terms(self.session)
+
+    def tearDown(self):
+        self.session.rollback()
+        for article in self.session.query(Document).all():
+            self.session.delete(article)
+        self.session.commit()
+
+    # def test_extract_facts_simple(self):
+    #     """Extracts simple facts when present and saves to DB"""
+    #     document = Document(type=DocumentType.WEB,
+    #                         name="Hurricane Katrina Fast Facts")
+    #     analysis = Analysis(document=document, status=Status.NEW)
+    #     self.session.add(analysis)
+    #     content = DocumentContent(
+    #         content="It was early Saturday when a flash flood hit the area and washed away more than 500 houses")
+    #     self.session.add(content)
+    #     self.session.commit()
+    #     analysis.content_id = content.id
+    #     self.session.commit()
+    #     extract_facts(analysis)
+    #     self.assertEqual(1, len(analysis.facts))
+
+    # def test_create_facts_per_country(self):
+    #     """Extracts simple facts when present and saves to DB"""
+    #     document = Document(type=DocumentType.WEB,
+    #                         name="Hurricane Katrina Fast Facts")
+    #     analysis = Analysis(document=document, status=Status.NEW)
+    #     self.session.add(analysis)
+    #     content = DocumentContent(
+    #         content="It was early Saturday when a flash flood hit large parts of Pakistan and India and washed away more than 500 houses")
+    #     self.session.add(content)
+    #     self.session.commit()
+    #     analysis.content_id = content.id
+    #     self.session.commit()
+    #     extract_facts(analysis)
+    #     facts = analysis.facts
+    #     self.assertEqual(2, len(facts))
+    #     iso_codes = [f.iso3 for f in facts]
+    #     self.assertIn('IND', iso_codes)
+    #     self.assertIn('PAK', iso_codes)
+
+
+    # def test_use_existing_locations_for_facts(self):
+    #     """Extracts simple facts when present and saves to DB"""
+    #     d1 = Document(type=DocumentType.WEB,
+    #                         name="Hurricane Katrina Fast Facts")
+    #     analysis1 = Analysis(document=d1, status=Status.NEW)
+    #     self.session.add(analysis1)
+    #     content = DocumentContent(
+    #         content="It was early Saturday when a flash flood hit large parts of India and washed away more than 500 houses")
+    #     self.session.add(content)
+    #     self.session.commit()
+    #     analysis1.content_id = content.id
+    #     self.session.commit()
+    #     extract_facts(analysis1)
+
+    #     d2 = Document(type=DocumentType.WEB,
+    #                         name="Hurricane Katrina Fast Facts")
+    #     analysis2 = Analysis(document=d2, status=Status.NEW)
+    #     self.session.add(analysis2)
+    #     content = DocumentContent(
+    #         content="It was early Saturday when a flash flood hit large parts of India and washed away more than 500 houses")
+    #     self.session.add(content)
+    #     self.session.commit()
+    #     analysis2.content_id = content.id
+    #     self.session.commit()
+    #     extract_facts(analysis2)
+
+    #     self.assertEqual(analysis1.facts[0].locations[0], analysis2.facts[0].locations[0])
+
+    # def test_process_location_return_existing(self):
+    #     country = self.session.query(Country).filter_by(
+    #             iso3='GBR').one_or_none()
+    #     location = Location(location_name='Ruislip', location_type='',
+    #                         country_iso3=country.iso3,
+    #                         country=country, latlong='')
+    #     self.session.add(location)
+    #     self.session.commit()
+
+
+# Test process_location
+# Returns existing location
+# Creates location if doesn't exists for existing country
