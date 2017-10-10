3bf0cecc40b226441408c048d2afbca292dea299
Simon Bedford <simon.bedford@gmail.com>
2017-10-10 15:08:01 +0100

    Update setup script for new models and add script for loading urls

--- source/python/setup.py
+++ source/python/setup.py
@@ -5,8 +5,13 @@ One-time setup script to download classifier models and pre-populate database wi
 from sqlalchemy import create_engine
 from idetect.model import db_url, Base, Session, Country, FactKeyword
 from idetect.load_data import load_countries, load_terms
-from idetect.nlp_models.category import CategoryModel, Tokenizer, TfidfTransformer, LsiTransformer
-from idetect.nlp_models.relevance import RelevanceModel, Stemmer, Combiner, RelevanceKeyWordClassifier
+import re
+import string
+import numpy as np
+import pandas as pd
+from idetect.nlp_models.category import * 
+from idetect.nlp_models.relevance import * 
+from idetect.nlp_models.base_model import CustomSklLsiModel
 
 if __name__ == "__main__":
 
@@ -17,15 +22,17 @@ if __name__ == "__main__":
 
     session = Session()
     # Load the Countries data if necessary
-    if not session.query(Country).first():
+    countries = session.query(Country).all()
+    if len(countries) == 0:
         load_countries(session)
 
     # Load the Keywords if neccessary
-    if not session.query(FactKeyword).first():
+    keywords = session.query(FactKeyword).all()
+    if len(keywords) == 0:
         load_terms(session)
 
     session.close()
 
     # Load the Classifier models once to ensure they are downloaded
-    CategoryModel()
-    RelevanceModel()
+    # CategoryModel()
+    # RelevanceModel()
