225a5d07d9cec92b83bfbeaca45ac9338971170a
George Richardson <g.raymond.richardson@gmail.com>
2017-10-13 17:31:02 +0200

    use original content for category

--- source/python/idetect/classifier.py
+++ source/python/idetect/classifier.py
@@ -6,14 +6,16 @@ from sqlalchemy.orm import object_session
 
 def classify(analysis, category_model, relevance_model):
     """
-    Classify article into category & relevance and save results to database
-    :params article: An Article instance
+    Tag and categorize analysis using its content.
+    
+    :params analysis: An Analysis instance
     :return: None
     """
     session = object_session(analysis)
-    content = analysis.content.content_clean
+    content = analysis.content.content
     category = category_model.predict(content)
-    relevance = relevance_model.predict(content)
+    content_clean = analysis.content.content_clean
+    relevance = relevance_model.predict(content_clean)
     analysis.category = category
     analysis.relevance = relevance
     session.commit()
