5fe2e0acf2d1affa63953294620a8b74f730575c
Simon Bedford <simon.bedford@gmail.com>
2017-10-17 15:54:37 -0500

    Add new worker for running geotagging process

--- source/python/idetect/model.py
+++ source/python/idetect/model.py
@@ -42,9 +42,12 @@ class Status:
     CLASSIFIED = 'classified'
     EXTRACTING = 'extracting'
     EXTRACTED = 'extracted'
+    GEOTAGGING = 'geotagging'
+    GEOTAGGED = 'geotagged'
     SCRAPING_FAILED = 'scraping failed'
     CLASSIFYING_FAILED = 'classifying failed'
     EXTRACTING_FAILED = 'extracting failed'
+    GEOTAGGING_FAILED = 'geotagging failed'
     EDITING = 'editing'
     EDITED = 'edited'
 
