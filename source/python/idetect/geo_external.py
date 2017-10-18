bceb01a746d7812aea45b83b6b7dafc834728e22
Simon Bedford <simon.bedford@gmail.com>
2017-10-18 11:28:16 -0500

    unit tests for new geotagging functions / worker

---
+++ source/python/idetect/geo_external.py
@@ -0,0 +1,51 @@
+import requests
+from idetect.model import LocationType
+
+class GeotagException(Exception):
+    pass
+
+
+def coords_tostring(coords_list, separator=','):
+    return separator.join(map(str, coords_list[::-1]))
+
+
+def mapzen_coordinates(place_name, country_code=None):
+    api_key = 'mapzen-i8JEmx7'
+    base_url = 'https://search.mapzen.com/v1/search'
+
+    query_params = {'text': place_name, 'api_key': api_key}
+    if country_code:
+        query_params['boundary.country'] = country_code
+    try:
+        resp = requests.get(base_url, params=query_params)
+        res = resp.json()
+        data = res["features"]
+        if len(data) == 0:
+            return {'place_name': place_name, 'type': '', 'country_code': country_code, 'flag': 'no-results', 'coordinates': ''}
+        else:
+            if len(data) > 1:
+                flag = "multiple-results"
+            else:
+                flag = "single-result"
+
+            data.sort(key=lambda x: x['properties']['confidence'], reverse=True)
+            return {'place_name': place_name, 'type': layer_to_entity(data[0]['properties']['layer']),
+                    'country_code': data[0]['properties']['country_a'], 'flag': flag,
+                    'coordinates': coords_tostring(data[0]['geometry']['coordinates'])}
+    except:
+        raise GeotagException()
+
+
+def layer_to_entity(layer):
+    if layer in ('address', 'street'):
+        return LocationType.ADDRESS
+    elif layer in ('neighbourhood', 'borough', 'localadmin'):
+        return LocationType.NEIGHBORHOOD
+    elif layer in ('locality'):
+        return LocationType.CITY
+    elif layer in ('county', 'region'):
+        return LocationType.SUBDIVISION
+    elif layer in ('country'):
+        return LocationType.COUNTRY
+    else:
+        return LocationType.UNKNOWN
--- source/python/idetect/tests/test_geo_tagging.py
+++ source/python/idetect/tests/test_geo_tagging.py
@@ -1,15 +1,16 @@
 import os
-from unittest import TestCase
+from unittest import TestCase, mock
 
 from sqlalchemy import create_engine
 
-from idetect.model import Base, Session, LocationType
+from idetect.model import Base, Session, Status, Document, Analysis, DocumentContent, DocumentType, Country, Location, LocationType, Fact
 from idetect.load_data import load_countries
-from idetect.geotagger import get_geo_info
+from idetect.fact_extractor import extract_facts
+from idetect.geotagger import get_geo_info, process_locations, mapzen_coordinates, GeotagException
 
 
 class TestGeoTagger(TestCase):
-
+ 
     def setUp(self):
         db_host = os.environ.get('DB_HOST')
         db_url = 'postgresql://{user}:{passwd}@{db_host}/{db}'.format(
@@ -52,5 +53,73 @@ class TestGeoTagger(TestCase):
         results = get_geo_info("Alaska")
         self.assertEqual(results['type'], LocationType.SUBDIVISION)
 
+    # DONT RUN geotagging if detail already exists
+    @mock.patch('idetect.geotagger.mapzen_coordinates')
+    def dont_geotag_if_detail_exists(self, mapzen):
+        document = Document(type=DocumentType.WEB,
+                            name="Hurricane Katrina Fast Facts")
+        analysis = Analysis(document=document, status=Status.NEW)
+        self.session.add(analysis)
+        content = DocumentContent(
+            content_clean="It was early Saturday when a flash flood hit large parts of India and Pakistan and washed away more than 500 houses")
+        self.session.add(content)
+        self.session.commit()
+        analysis.content_id = content.id
+        self.session.commit()
+        fact = Fact(unit='person', term='displaced')
+        self.session.add(fact)
+        self.session.commit()
+        loc1 = self.session.query(Location).filter(Location.location_name == 'India').one_or_none()
+        fact.locations.append(loc1)
+        analysis.facts.append(fact)
+        self.session.commit()
+        process_locations(analysis)
+        assert not mapzen.called
+
+
+    def test_create_duplicate_fact(self):
+        """Creates duplicate fact if locations from multiple countries exist"""
+        document = Document(type=DocumentType.WEB,
+                            name="Hurricane Katrina Fast Facts")
+        analysis = Analysis(document=document, status=Status.NEW)
+        self.session.add(analysis)
+        self.session.commit()
+        fact = Fact(unit='person', term='displaced')
+        self.session.add(fact)
+        self.session.commit()
+        loc1 = self.session.query(Location).filter(Location.location_name == 'India').one_or_none()
+        loc2 = self.session.query(Location).filter(Location.location_name == 'Pakistan').one_or_none()
+        fact.locations.append(loc1)
+        fact.locations.append(loc2)
+        analysis.facts.append(fact)
+        self.session.commit()
+        self.assertEqual(1, len(analysis.facts))
+        process_locations(analysis)
+        self.assertEqual(2, len(analysis.facts))
+        fact_countries = [f.iso3 for f in analysis.facts]
+        self.assertIn('IND', fact_countries)
+        self.assertIn('PAK', fact_countries)
+        self.assertEqual(1, len(analysis.facts[0].locations))
+        self.assertEqual(1, len(analysis.facts[1].locations))
+
+
+    @mock.patch('idetect.geotagger.mapzen_coordinates')
+    def test_fail_if_geotagging_fails(self, mapzen):
+        """Location processing should fail if geotagging fails"""
+        mapzen.side_effect = GeotagException()
+        document = Document(type=DocumentType.WEB,
+                            name="Hurricane Katrina Fast Facts")
+        analysis = Analysis(document=document, status=Status.NEW)
+        self.session.add(analysis)
+        self.session.commit()
+        fact = Fact(unit='person', term='displaced')
+        self.session.add(fact)
+        self.session.commit()
+        loc1 = Location(location_name="Ruislip")
+        fact.locations.append(loc1)
+        analysis.facts.append(fact)
+        self.session.commit()
+        with self.assertRaises(GeotagException):
+            process_locations(analysis)
 
 
