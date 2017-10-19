3801fa81499b86188b1ae4e28ac17704768d7ffb
Simon Bedford <simon.bedford@gmail.com>
2017-10-19 18:56:26 -0500

    SQL for adding new keywords

---
+++ db/add_refugee_keywords.sql
@@ -0,0 +1,2 @@
+INSERT INTO idetect_fact_keywords(description, keyword_type)
+VALUES ('refugee camp', 'person_term'), ('refugee center', 'person_term'), ('refugee', 'person_unit'), ('asylum seeker', 'person_unit');
