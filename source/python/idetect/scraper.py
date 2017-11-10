07daa3f27e84a652dc72588b9684b1da3571bff2
Simon Bedford <simon.bedford@gmail.com>
2017-11-10 12:55:28 -0600

    Commit after setting language and add exceptions to pdf scraping

--- source/python/idetect/scraper.py
+++ source/python/idetect/scraper.py
@@ -108,6 +108,7 @@ def scrape_html(analysis):
         except LangDetectException:
             raise Exception("Unable to determine language")
         if analysis.language != 'en':
+            session.commit()
             raise Exception("Article not in English")
         content = DocumentContent(analysis=[analysis], content=text, content_clean=text_clean, content_type='text')
         session = object_session(analysis)
@@ -152,7 +153,10 @@ def scrape_pdf(url, analysis):
         try:
             analysis.language = detect(text)
         except LangDetectException:
-            pass
+            raise Exception("Unable to determine language")
+        if analysis.language != 'en':
+            session.commit()
+            raise Exception("Article not in English")
         content = DocumentContent(analysis=[analysis], content=text, content_clean=text_clean, content_type='pdf')
         session = object_session(analysis)
         session.add(content)
